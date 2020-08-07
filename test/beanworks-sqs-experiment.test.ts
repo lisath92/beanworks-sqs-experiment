5661c4cbbaba1f49507ffaa6a896b93f5bbdafbf
Lisa Hsu <lisa.hsu.419@gmail.com>
2020-08-06 22:05:27 -0700

    add tests

--- test/beanworks-sqs-experiment.test.ts
+++ test/beanworks-sqs-experiment.test.ts
@@ -1,21 +1,51 @@
-import { expect as expectCDK, haveResource } from '@aws-cdk/assert';
+import { expect as expectCDK, haveResource, haveResourceLike, arrayWith, objectLike, countResources, anything } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
 import * as BeanworksSqsExperiment from '../lib/beanworks-sqs-experiment-stack';
 
 test('SQS Queue Created', () => {
     const app = new cdk.App();
-    // WHEN
-    const stack = new BeanworksSqsExperiment.BeanworksSqsExperimentStack(app, 'MyTestStack');
-    // THEN
+    const stack = new BeanworksSqsExperiment.BeanworksSqsExperimentStack(app, 'TestStack');
+
     expectCDK(stack).to(haveResource("AWS::SQS::Queue",{
-      VisibilityTimeout: 300
+      VisibilityTimeout: 10,
+      FifoQueue: true,
+      ContentBasedDeduplication: true
     }));
 });
 
-test('SNS Topic Created', () => {
+test('Both Lambda Functions Created', () => {
+    const app = new cdk.App();
+    const stack = new BeanworksSqsExperiment.BeanworksSqsExperimentStack(app, 'TestStack');
+
+    expectCDK(stack).to(countResources("AWS::Lambda::Function", 2));
+})
+
+test('Both Lambda Functions Created with Properties', () => {
   const app = new cdk.App();
-  // WHEN
-  const stack = new BeanworksSqsExperiment.BeanworksSqsExperimentStack(app, 'MyTestStack');
-  // THEN
-  expectCDK(stack).to(haveResource("AWS::SNS::Topic"));
-});
+  const stack = new BeanworksSqsExperiment.BeanworksSqsExperimentStack(app, 'TestStack');
+
+  expectCDK(stack).to(haveResource("AWS::Lambda::Function", {
+    Timeout: 15
+  }))
+  expectCDK(stack).to(haveResource("AWS::Lambda::Function", {
+    Environment:{
+      Variables: {
+        SQS_URL: anything()
+      }
+    }
+  }))
+})
+
+test('Lambda Function with SQS Policy Created', () => {
+  const app = new cdk.App();
+  const stack = new BeanworksSqsExperiment.BeanworksSqsExperimentStack(app, 'TestStack');
+
+  expectCDK(stack).to(haveResourceLike('AWS::IAM::Policy', {
+    PolicyDocument: {
+      Statement: arrayWith(objectLike({
+        Action: 'sqs:SendMessage',
+        Resource: anything()
+      }))
+    }
+  }))
+})
